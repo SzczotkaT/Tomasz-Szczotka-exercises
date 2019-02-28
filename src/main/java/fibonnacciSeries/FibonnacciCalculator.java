2fa54015818e8c06ed31e06ea9c78bb81d2f4386
SzczotkaT <tomasz.szczotka89@gmail.com>
2019-02-28 14:56:53 +0100

    Fibonacci calculator

--- src/main/java/fibonnacciSeries/FibonnacciCalculator.java
+++ src/main/java/fibonnacciSeries/FibonnacciCalculator.java
@@ -12,6 +12,12 @@ public class FibonnacciCalculator {
         for (int i = 1; i <= number; i++) {
             System.out.print(fibonacciNumber(i) + " ");
         }
+        System.out.println();
+        System.out.println("The Fibonacci series up to : " + number + " numbers using for loop: ");
+
+        for (int i = 1; i <= number; i++){
+            System.out.print(fibonacciNumber2(i) + " ");
+        }
     }
 
     public static int fibonacciNumber(int number){
@@ -19,5 +25,19 @@ public class FibonnacciCalculator {
             return 1;
         }
         return fibonacciNumber(number - 1) + fibonacciNumber(number - 2);
+    }
+
+    public static int fibonacciNumber2(int number){
+        if(number == 1 || number == 2){
+            return 1;
+        }
+        int num1 = 1, num2 = 1, fibonacci = 1;
+
+        for (int i = 3; i <= number; i++) {
+            fibonacci = num1 + num2;
+            num1 = num2;
+            num2 = fibonacci;
+        }
+        return fibonacci;
     }
 }
